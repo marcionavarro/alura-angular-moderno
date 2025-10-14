18e857b751f6aa2b46d1174270bbbfcad0408c34
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2025-10-13 23:09:37 -0300

    01-Explore o Framework Angular[ 04-Angular ciclo de vida ] 02-Dando vida aos componentes

--- life-cycle/src/app/components/item/item.component.ts
+++ life-cycle/src/app/components/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { Item } from 'src/app/interfaces/iItem';
 
@@ -7,14 +7,22 @@ import { Item } from 'src/app/interfaces/iItem';
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit {
+export class ItemComponent implements OnInit, OnChanges {
   @Input() item!: Item;
-  
+
   faPen = faPen;
   faTrash = faTrash
 
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    console.log("🚀 ~ ItemComponent ~ ngOnInit:")
+  }
+
+
+  ngOnChanges(): void {
+    console.log("🚀 ~ ItemComponent ~ ngOnChanges:")
+  }
+
 
 }
