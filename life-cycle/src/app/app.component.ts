dec6aaac0e3c416456a34f96226f8679e751618f
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2025-10-13 23:52:36 -0300

    01-Explore o Framework Angular[ 04-Angular ciclo de vida ] 04-Trabalhando com dados reais

--- life-cycle/src/app/app.component.ts
+++ life-cycle/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { Item } from './interfaces/iItem';
 import { ListaDeCompraService } from './service/lista-de-compra.service';
 
@@ -7,7 +7,7 @@ import { ListaDeCompraService } from './service/lista-de-compra.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, DoCheck {
   title = 'app-lista-de-compras';
   listaDeCompra!: Array<Item>
   itemParaSerEditado!: Item;
@@ -16,11 +16,15 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.listaDeCompra = this.listaService.getListaDeCompra();
-    console.log(this.listaDeCompra);
   }
 
   editarItem(item: Item) {
     this.itemParaSerEditado = item;
+  }
+
+  ngDoCheck(): void {
+    console.log('DoCheck foi chamado');
+    this.listaService.atualizarLocalStorage();
   }
 
 }
