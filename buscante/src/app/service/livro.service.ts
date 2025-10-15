0b8484e5cc412f9c713ad82ce3a23610b13ad720
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2025-10-14 23:25:37 -0300

    01-Explore o Framework Angular[ 05-RxJS e Angular programando de forma reativa ] 03-Operadores RxJS

--- buscante/src/app/service/livro.service.ts
+++ buscante/src/app/service/livro.service.ts
@@ -1,17 +1,23 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
+import { Item, LivrosResultado } from '../modulos/interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LivroService {
   private readonly API = 'https://www.googleapis.com/books/v1/volumes'
-  
+
   constructor(private http: HttpClient) { }
 
-  buscar(valorDigitado: string): Observable<any> {
+  buscar(valorDigitado: string): Observable<Item[]> {
     const params = new HttpParams().append('q', valorDigitado);
-    return this.http.get(this.API, { params });
+    return this.http.get<LivrosResultado>(this.API, { params })
+      .pipe(
+        tap((retornoAPI) => console.log(retornoAPI)),
+        map(resultado => resultado.items),
+        tap((resultado) => console.log(resultado)),
+      );
   }
 }
--- buscante/src/app/views/lista-livros/lista-livros.component.ts
+++ buscante/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { Livro } from 'src/app/modulos/interfaces';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -9,18 +10,36 @@ import { LivroService } from 'src/app/service/livro.service';
 })
 export class ListaLivrosComponent implements OnDestroy {
 
-  listaLivros: [];
+  listaLivros: Livro[];
   campoBusca: string = '';
   subscription: Subscription;
+  livro: Livro;
 
   constructor(private service: LivroService) { }
 
   buscarLivros() {
     this.subscription = this.service.buscar(this.campoBusca).subscribe({
-      next: retornoAPI => console.log(retornoAPI),
-      error: erro => console.error(erro),
-      complete: () => console.log('Observable Completado')
+      next: items => this.listaLivros = this.livrosResultadoParaLivros(items),
+      error: erro => console.error(erro)
     });
+  }
+
+  livrosResultadoParaLivros(items) {
+    const livros: Livro[] = [];
+
+    items.forEach(item => {
+      livros.push(this.livro = {
+        title: item.volumeInfo?.title,
+        authors: item.volumeInfo?.authors,
+        publisher: item.volumeInfo?.publisher,
+        publishedDate: item.volumeInfo?.publishedDate,
+        description: item.volumeInfo?.description,
+        previewLink: item.volumeInfo?.previewLink,
+        thumbnail: item.volumeInfo?.imageLinks?.thumbnail
+      })
+    });
+
+    return livros;
   }
 
   ngOnDestroy() {
