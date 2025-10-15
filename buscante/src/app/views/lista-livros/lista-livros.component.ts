efbdbae0a00940993ce35277ca4a86617d073a46
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2025-10-15 00:53:51 -0300

    01-Explore o Framework Angular[ 05-RxJS e Angular programando de forma reativa ] 06-Tratamento de erros

--- buscante/src/app/views/lista-livros/lista-livros.component.ts
+++ buscante/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { debounceTime, filter, map, switchMap, tap } from 'rxjs';
-import { Item, Livro } from 'src/app/models/interfaces';
+import { catchError, debounceTime, EMPTY, filter, map, of, switchMap, tap, throwError } from 'rxjs';
+import { Item, Livro, LivrosResultado } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/LivroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
@@ -11,12 +11,25 @@ const PAUSA = 300;
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent{
+export class ListaLivrosComponent {
   campoBusca = new FormControl();
-  livro: Livro;
-
+  mensagemErro: string = '';
+  livrosResultado: LivrosResultado;
 
   constructor(private service: LivroService) { }
+
+  /* totalDeLivros$ = this.campoBusca.valueChanges
+    .pipe(
+      debounceTime(PAUSA),
+      filter(valorDigitado => valorDigitado.length >= 3),
+      tap(() => console.log('Fluxo inicial')),
+      switchMap(valorDigitado => this.service.buscar(valorDigitado)),
+      map(resultado => this.livrosResultado = resultado),
+      catchError(erro => {
+        console.log(erro);
+        return of();
+      })
+    ); */
 
   livrosEncontrados$ = this.campoBusca.valueChanges
     .pipe(
@@ -24,8 +37,16 @@ export class ListaLivrosComponent{
       filter(valorDigitado => valorDigitado.length >= 3),
       tap(() => console.log('Fluxo inicial')),
       switchMap(valorDigitado => this.service.buscar(valorDigitado)),
+      map(resultado => this.livrosResultado = resultado),
       tap((retornoAPI) => console.log(retornoAPI)),
-      map(items => this.livrosResultadoParaLivros(items))
+      map(resultado => resultado.items ?? []),
+      map(items => this.livrosResultadoParaLivros(items)),
+      catchError((erro) => {
+        /* this.mensagemErro = 'Ops, ocorreu um erro, recarregue a aplicação!'
+        return EMPTY; */
+        console.log(erro);
+        return throwError(() => new Error(this.mensagemErro = 'Ops, ocorreu um erro, recarregue a aplicação!'))
+      })
     )
 
   livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
