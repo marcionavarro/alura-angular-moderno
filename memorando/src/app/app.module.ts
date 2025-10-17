f8ca5fc34e64d57ab4567653e6bd014d9d15c655
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2025-10-16 22:15:59 -0300

    01-Explore o Framework Angular[ 06-Angular torne sua aplicação interativa e personalizada com animações ] 01-Conhecendo as animações

--- memorando/src/app/app.module.ts
+++ memorando/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { ListaTarefasComponent } from './lista-tarefas/lista-tarefas.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
-import { RodapeComponent } from './componentes/rodape/rodape.component';
 import { MensagemComponent } from './componentes/mensagem/mensagem.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { ListaTarefasComponent } from './lista-tarefas/lista-tarefas.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { MensagemComponent } from './componentes/mensagem/mensagem.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
