7dc5e9deaeecf0e4154198b008dac455d1a73e27
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2025-10-20 23:17:24 -0300

    01-Explore o Framework Angular[ 07-Angular aprimore suas técnicas de animação e crie interfaces ainda mais atraentes ] 03-Chamando atenção com query e keyframes

--- memorando/src/app/lista-tarefas/lista-tarefas.component.ts
+++ memorando/src/app/lista-tarefas/lista-tarefas.component.ts
@@ -4,17 +4,31 @@ import { Router } from '@angular/router';
 
 import { TarefaService } from 'src/app/service/tarefa.service';
 import { Tarefa } from '../interface/tarefa';
-import { checkButtonTrigger, filterTrigger, formButtonTrigger, highlightedStateTrigger, shownStateTrigger } from '../animations';
+import {
+  checkButtonTrigger,
+  filterTrigger,
+  formButtonTrigger,
+  highlightedStateTrigger,
+  shakeTrigger,
+  shownStateTrigger
+} from '../animations';
 
 @Component({
   selector: 'app-lista-tarefas',
   templateUrl: './lista-tarefas.component.html',
   styleUrls: ['./lista-tarefas.component.css'],
-  animations: [highlightedStateTrigger, shownStateTrigger, checkButtonTrigger, filterTrigger, formButtonTrigger]
+  animations: [
+    highlightedStateTrigger,
+    shownStateTrigger,
+    checkButtonTrigger,
+    filterTrigger,
+    formButtonTrigger,
+    shakeTrigger
+  ]
 })
 export class ListaTarefasComponent implements OnInit {
   listaTarefas: Tarefa[] = [];
-  formAberto: boolean = true;
+  formAberto: boolean = false;
   categoria: string = '';
   validado: boolean = false;
   indexTarefa = -1;
