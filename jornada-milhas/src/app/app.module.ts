bec38e1efafa30a6294a68f3e21a6886eb45a1e8
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2025-10-28 00:12:25 -0300

    02-Desenvolva Aplicações Escaláveis com Angular[ 01-Angular componentização e design com Angular Material ] 02-Criando componentes reutilizáveis

--- jornada-milhas/src/app/app.module.ts
+++ jornada-milhas/src/app/app.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
 import { BannerComponent } from './shared/banner/banner.component';
+import { CardBuscaComponent } from './shared/card-busca/card-busca.component';
 import { CardComponent } from './shared/card/card.component';
+import { ContainerComponent } from './shared/container/container.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { CardDepoimentoComponent } from './shared/card-depoimento/card-depoimento.component';
 
 @NgModule({
   declarations: [
@@ -17,14 +22,19 @@ import { HeaderComponent } from './shared/header/header.component';
     HeaderComponent,
     FooterComponent,
     BannerComponent,
-    CardComponent
+    CardComponent,
+    ContainerComponent,
+    HomeComponent,
+    CardBuscaComponent,
+    CardDepoimentoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
