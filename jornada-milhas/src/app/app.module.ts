ebe26a1a76e9fdb681dbf14a01a48094c04ba9dc
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2025-11-03 22:13:15 -0300

    02-Desenvolva Aplicações Escaláveis com Angular[ 05-Angular boas práticas de desenvolvimento com Modularização, Lazy Loading e Interceptors ] 01-Entendendo a arquitetura modular

--- jornada-milhas/src/app/app.module.ts
+++ jornada-milhas/src/app/app.module.ts
@@ -1,33 +1,12 @@
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatChipsModule } from '@angular/material/chips';
-import { MAT_DATE_FORMATS, MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { BannerComponent } from './shared/banner/banner.component';
-import { CardBuscaComponent } from './shared/card-busca/card-busca.component';
-import { CardDepoimentoComponent } from './shared/card-depoimento/card-depoimento.component';
-import { CardComponent } from './shared/card/card.component';
-import { ContainerComponent } from './shared/container/container.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { FormBuscaComponent } from './shared/form-busca/form-busca.component';
-import { HeaderComponent } from './shared/header/header.component';
+
 
 // Para registrar o locale
 import { registerLocaleData } from '@angular/common';
@@ -35,26 +14,16 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import localePt from '@angular/common/locales/pt';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './pages/login/login.component';
-import { BotaoControleComponent } from './shared/botao-controle/botao-controle.component';
-import { FormBaseComponent } from './shared/form-base/form-base.component';
 
-import { MatSliderModule } from '@angular/material/slider';
+
 import { AutenticacaoInterceptor } from './core/interceptors/autenticacao.interceptor';
 import { BuscaComponent } from './pages/busca/busca.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { DepoimentosComponent } from './pages/home/depoimentos/depoimentos.component';
 import { PromocoesComponent } from './pages/home/promocoes/promocoes.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
-import { DropdownUfComponent } from './shared/dropdown-uf/dropdown-uf.component';
-import { CompanhiasComponent } from './shared/form-busca/filtros-complementares/companhias/companhias.component';
-import { FiltrosComplementaresComponent } from './shared/form-busca/filtros-complementares/filtros-complementares.component';
-import { LabelComponent } from './shared/form-busca/filtros-complementares/label/label.component';
-import { ParadasComponent } from './shared/form-busca/filtros-complementares/paradas/paradas.component';
-import { PrecosComponent } from './shared/form-busca/filtros-complementares/precos/precos.component';
-import { ModalComponent } from './shared/modal/modal.component';
-import { PassagemComponent } from './shared/passagem/passagem.component';
-import { SeletorPassageiroComponent } from './shared/seletor-passageiro/seletor-passageiro.component';
-import { PassagemDestaqueComponent } from './shared/passagem-destaque/passagem-destaque.component';
+import { SharedModule } from './shared/shared.module';
+import { MaterialModule } from './core/material/material.module';
 registerLocaleData(localePt);
 
 export const MY_DATE_FORMATS = {
@@ -72,56 +41,22 @@ export const MY_DATE_FORMATS = {
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    BannerComponent,
-    CardComponent,
-    ContainerComponent,
     HomeComponent,
-    CardBuscaComponent,
-    CardDepoimentoComponent,
-    FormBuscaComponent,
-    ModalComponent,
-    BotaoControleComponent,
-    DropdownUfComponent,
-    SeletorPassageiroComponent,
     LoginComponent,
-    FormBaseComponent,
     CadastroComponent,
     DepoimentosComponent,
     PromocoesComponent,
     PerfilComponent,
     BuscaComponent,
-    PassagemComponent,
-    FiltrosComplementaresComponent,
-    PrecosComponent,
-    ParadasComponent,
-    LabelComponent,
-    CompanhiasComponent,
-    PassagemDestaqueComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatCardModule,
-    MatButtonToggleModule,
-    MatIconModule,
-    MatChipsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatDialogModule,
+    BrowserModule,
     HttpClientModule,
+    MaterialModule,
     ReactiveFormsModule,
-    MatAutocompleteModule,
-    MatRadioModule,
-    MatDividerModule,
-    MatCheckboxModule,
-    MatSliderModule
+    SharedModule,
   ],
   providers: [
     {
---
+++ jornada-milhas/src/app/shared/shared.module.ts
@@ -0,0 +1,79 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BannerComponent } from './banner/banner.component';
+import { BotaoControleComponent } from './botao-controle/botao-controle.component';
+import { CardBuscaComponent } from './card-busca/card-busca.component';
+import { CardDepoimentoComponent } from './card-depoimento/card-depoimento.component';
+import { CardComponent } from './card/card.component';
+import { ContainerComponent } from './container/container.component';
+import { DropdownUfComponent } from './dropdown-uf/dropdown-uf.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormBaseComponent } from './form-base/form-base.component';
+import { CompanhiasComponent } from './form-busca/filtros-complementares/companhias/companhias.component';
+import { FiltrosComplementaresComponent } from './form-busca/filtros-complementares/filtros-complementares.component';
+import { LabelComponent } from './form-busca/filtros-complementares/label/label.component';
+import { ParadasComponent } from './form-busca/filtros-complementares/paradas/paradas.component';
+import { PrecosComponent } from './form-busca/filtros-complementares/precos/precos.component';
+import { FormBuscaComponent } from './form-busca/form-busca.component';
+import { HeaderComponent } from './header/header.component';
+import { ModalComponent } from './modal/modal.component';
+import { PassagemDestaqueComponent } from './passagem-destaque/passagem-destaque.component';
+import { PassagemComponent } from './passagem/passagem.component';
+import { SeletorPassageiroComponent } from './seletor-passageiro/seletor-passageiro.component';
+import { MaterialModule } from '../core/material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    BannerComponent,
+    BotaoControleComponent,
+    CardBuscaComponent,
+    CardDepoimentoComponent,
+    CardComponent,
+    ContainerComponent,
+    FormBaseComponent,
+    DropdownUfComponent,
+    CompanhiasComponent,
+    FiltrosComplementaresComponent,
+    FooterComponent,
+    FormBuscaComponent,
+    HeaderComponent,
+    LabelComponent,
+    ParadasComponent,
+    PrecosComponent,
+    ModalComponent,
+    PassagemComponent,
+    SeletorPassageiroComponent,
+    PassagemDestaqueComponent
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    ReactiveFormsModule,
+  ],
+  exports: [
+    BannerComponent,
+    BotaoControleComponent,
+    CardBuscaComponent,
+    CardDepoimentoComponent,
+    CardComponent,
+    ContainerComponent,
+    FormBaseComponent,
+    DropdownUfComponent,
+    CompanhiasComponent,
+    FiltrosComplementaresComponent,
+    FooterComponent,
+    FormBuscaComponent,
+    HeaderComponent,
+    LabelComponent,
+    ParadasComponent,
+    PrecosComponent,
+    ModalComponent,
+    PassagemComponent,
+    SeletorPassageiroComponent,
+    PassagemDestaqueComponent
+  ]
+})
+export class SharedModule { }
