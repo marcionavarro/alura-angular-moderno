f1d4aa915848f4750a5da9791022373cb38027a9
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2026-03-07 15:07:31 -0300

    03-Angular cursos de testes automatizados para garantir a qualidade da sua aplicação[ 02-Angular explorando testes de unidade e integração ] 02-Fundamentos de testes no Angular

--- zoop-store/src/app/shared/components/card/card.component.spec.ts
+++ zoop-store/src/app/shared/components/card/card.component.spec.ts
@@ -1,8 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CardComponent } from './card.component';
+import { Product } from '../../../types/product.inteface';
+import { By } from '@angular/platform-browser';
 
-describe('CardComponent', () => {
+fdescribe('CardComponent', () => {
   let component: CardComponent;
   let fixture: ComponentFixture<CardComponent>;
 
@@ -10,8 +12,8 @@ describe('CardComponent', () => {
     await TestBed.configureTestingModule({
       imports: [CardComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(CardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +21,83 @@ describe('CardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve renderizar o produto no template', () => {
+    const product: Product = {
+      id: 1,
+      title: 'IPhone 15',
+      price: 1000,
+      category: 'eletronico',
+      description: 'Smarth Phone',
+      image: 'src/assets/image.png'
+    }
+
+    component.product = product;
+    fixture.detectChanges();
+
+    const productImg = fixture.debugElement.query(By.css('img')).nativeElement;
+    const productTitle = fixture.debugElement.query(By.css('h2')).nativeElement;
+    const productDescription= fixture.debugElement.query(By.css('p')).nativeElement;
+    const productPrice = fixture.debugElement.query(By.css('h3')).nativeElement;
+
+    expect(productImg.src).toContain(product.image);
+    expect(productTitle.textContent).toContain(product.title);
+    expect(productDescription.textContent).toContain(product.description);
+    expect(productPrice.textContent).toContain(product.price);
+  });
+
+  it('deve emitir o evento onDelete quando clicar no delete', () => {
+    const product: Product = {
+      id: 2,
+      title: 'Samsung s22',
+      price: 950,
+      category: 'eletronico',
+      description: 'Smarth Phone',
+      image: 'src/assets/image.png'
+    }
+
+    component.product = product;
+    fixture.detectChanges();
+
+    const spy = spyOn(component.onDelete, 'emit');
+    
+    component.isManagable = true;
+
+    fixture.detectChanges();
+
+    const managableElement = fixture.debugElement.query(By.css('span')).nativeElement;
+    expect(managableElement).not.toBeNull();
+
+    component.onDeleteClick();
+    expect(spy).toHaveBeenCalledWith(product);
+
+  });
+
+  it('deve emitir o evento onEdit quando clicar no edit', () => {
+    const product: Product = {
+      id: 2,
+      title: 'Samsung s22',
+      price: 950,
+      category: 'eletronico',
+      description: 'Smarth Phone',
+      image: 'src/assets/image.png'
+    }
+
+    component.product = product;
+    fixture.detectChanges();
+
+    const spy = spyOn(component.onEdit, 'emit');
+    
+    component.isManagable = true;
+
+    fixture.detectChanges();
+
+    const managableElement = fixture.debugElement.query(By.css('span')).nativeElement;
+    expect(managableElement).not.toBeNull();
+
+    component.onEditClick();
+    expect(spy).toHaveBeenCalledWith(product);
+
   });
 });
