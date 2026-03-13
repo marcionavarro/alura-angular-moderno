24c732f9b9d7d497de2bbb850fd37de37f345e0a
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2026-03-12 22:10:36 -0300

    03-Angular cursos de testes automatizados para garantir a qualidade da sua aplicação[ 02-Angular explorando testes de unidade e integração ] 04-Teste de Services Finalizado

--- zoop-store/src/app/shared/services/products/products-api.service.spec.ts
+++ zoop-store/src/app/shared/services/products/products-api.service.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProductsApiService } from './products-api.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Product } from '../../../types/product.inteface';
+import { environment } from '../../../../environments/environment';
 
 describe('ProductsApiService', () => {
-  let service: ProductsApiService;
+  let httpTestingController: HttpTestingController;
+  let productsApiService: ProductsApiService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -13,10 +16,40 @@ describe('ProductsApiService', () => {
         ProductsApiService
       ]
     });
-    service = TestBed.inject(ProductsApiService);
+    productsApiService = TestBed.inject(ProductsApiService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(productsApiService).toBeTruthy();
+  });
+
+  it('deve retornar um Observable<Product[]> ao chamar getAllProducts', () => {
+    const itemPerPage = 5;
+    const mockProducts: Product[] = [
+      { id: 1, title: 'Produto A', category: 'eletronic', description: 'Product A', price: 50, image: 'image.png' },
+      { id: 2, title: 'Produto B', category: `woman's clothes`, description: 'Product B', price: 80, image: 'image.png' }
+    ]
+
+    productsApiService.getAllProducts(itemPerPage).subscribe((products: Product[]) => {
+      expect(products).toEqual(mockProducts);
+    });
+  });
+
+  it('deve lidar com erro ao chamar getAllProducts', () => {
+    const itemPerPage = 5;
+    const errorMessage = 'Erro ao carregar produtos';
+
+    productsApiService.getAllProducts(itemPerPage).subscribe(
+      () => fail('esperava-se um erro, mas obteve sucesso'),
+      (error: any) => {
+        expect(error).toBeTruthy();
+        expect(error.error).toEqual(errorMessage)
+      }
+    );
+
+    const req = httpTestingController.expectOne(`${environment.apiUrl}/products?limit=${itemPerPage}`);
+    expect(req.request.method).toEqual('GET');
+    req.flush(errorMessage, { status: 404, statusText: 'Not Found' });
   });
 });
--- zoop-store/src/app/shared/services/storage/storage.service.spec.ts
+++ zoop-store/src/app/shared/services/storage/storage.service.spec.ts
@@ -13,4 +13,79 @@ describe('StorageService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('save', () => {
+    it('deve salvar os dados no sessionStorage', () => {
+      const key = 'testKey';
+      const value = { name: 'test' }
+      service.save(key, { key, value });
+
+      const storedValue = JSON.parse(sessionStorage.getItem(key)!);
+      expect(storedValue).toEqual(value);
+    });
+  });
+
+  describe('get', () => {
+    it('deve obter os dados de sessionStorage', () => {
+      const key = 'testKey1';
+      const value = { name: 'test1' }
+      sessionStorage.setItem(key, JSON.stringify(value));
+
+      const storedValue = service.get(key);
+      expect(JSON.stringify(storedValue)).toEqual(JSON.stringify(value));
+    });
+
+    it('deve retornar null se os dados não forem encontrados na sessionStorage', () => {
+      const storedValue = service.get('nonExistentKey');
+      expect(storedValue).toBeNull();
+    })
+  });
+
+  describe('getAll', () => {
+    it('deve obter todos os dados da sessionStorage', () => {
+      sessionStorage.clear();
+
+      const data1 = { id: 1, name: 'Test 1' };
+      const data2 = { id: 2, name: 'Test 2' };
+      sessionStorage.setItem('1', JSON.stringify(data1));
+      sessionStorage.setItem('2', JSON.stringify(data2));
+
+      const storedData = service.getAll();
+      expect(storedData).toEqual([{ id: 1, name: 'Test 1' }, { id: 2, name: 'Test 2' }]);
+    })
+  });
+
+  describe('getLastItem', () => {
+    it('deve retornar o último item armazenado no sessionStorage', () => {
+      sessionStorage.clear();
+
+      const data1 = { id: 1, name: 'Test 1' };
+      const data2 = { id: 2, name: 'Test 2' };
+      sessionStorage.setItem('1', JSON.stringify(data1));
+      sessionStorage.setItem('2', JSON.stringify(data2));
+
+      const lastItem = service.getLastItem();
+      expect(lastItem).toEqual({ key: '2', value: { id: 2, name: 'Test 2' } });
+    });
+
+    it('deve retonar null se a sessionStorage estiver vazia', () => {
+      sessionStorage.clear();
+
+      const lastItem = service.getLastItem();
+      expect(lastItem).toBeNull();
+    })
+  });
+
+  describe('remove', () => {
+    it('deve remover os dados da sessionStorage', () => {
+      const key = 'testKey';
+      const value = { name: 'test' };
+
+      sessionStorage.setItem(key, JSON.stringify(value));
+      service.remove(key);
+
+      const storedValue = sessionStorage.getItem(key);
+      expect(storedValue).toBeNull();
+    })
+  });
 });
