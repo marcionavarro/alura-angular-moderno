b68c335cb62bff3a3f1f181b34f52872a22b455f
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2026-03-22 18:43:21 -0300

    03-Angular cursos de testes automatizados para garantir a qualidade da sua aplicação[ 03-Angular escreva testes end-to-end com Playwright ] 04-Jornadas de usuário - Finalizado

---
+++ jornada-milhas-playwright/e2e/fazerLogin.spec.ts
@@ -0,0 +1,21 @@
+import test from "@playwright/test";
+import { PaginaLogin } from "./page-objects/PaginaLogin";
+
+
+test.describe('Página de Login', () => {
+    test('Deve conseguir fazer login com email e senha válidos', async ({ page }) => {
+        const paginaLogin = new PaginaLogin(page);
+
+        await paginaLogin.visitar();
+        await paginaLogin.fazerLogin('marcionavarro@email.com', 'admin123');
+        await paginaLogin.loginFeitoComSucesso();
+    });
+
+    test('Não deve conseguir fazer login com email inválido', async ({ page }) => {
+        const paginaLogin = new PaginaLogin(page);
+
+        await paginaLogin.visitar();
+        await paginaLogin.fazerLogin('marcionavarro@errado.com', 'admin123');
+        await paginaLogin.estaMostrandoMensagemDeErro('Você não está autorizado a acessar este recurso');
+    });
+})
---
+++ jornada-milhas-playwright/e2e/jornadaBuscarPassagens.spec.ts
@@ -0,0 +1,19 @@
+import test from "@playwright/test";
+import { PaginaPrincipal } from "./page-objects/PaginaPrincipal";
+
+test.describe('Buscar passagens', () => {
+    test('Deve buscar passagem de somente ida', async ({ page }) => {
+        const paginaPrincipal = new PaginaPrincipal(page);
+
+        await paginaPrincipal.visitar();
+        await paginaPrincipal.definirSomenteIda();
+
+        await paginaPrincipal.abrirModalPassageiros();
+        await paginaPrincipal.definirPassageirosAdultos(3);
+        await paginaPrincipal.definirPassageirosCriancas(1);
+        await paginaPrincipal.definirPassageirosBebes(1);
+        await paginaPrincipal.fecharModalPassageiros();
+
+        await paginaPrincipal.definirOrigemEDestino('minas gerais', 'rio de janeiro');
+    });
+})
