4161c679597490ead82a7e0856600a3e3cd7eb84
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2026-06-12 22:42:42 -0300

    04-Angular avance com service workers, signals e SSR [ 01-Angular transformando uma aplicação web em PWA ] 01-Transformando o app em uma PWA - Finalizado

--- ng-fokus/src/app/app.config.ts
+++ ng-fokus/src/app/app.config.ts
@@ -9,5 +9,8 @@ export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration(), provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
+    }), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
     })]
 };
