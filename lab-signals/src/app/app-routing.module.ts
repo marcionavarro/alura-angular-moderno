577afe37906509e623827d657658dc1f983e03a7
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2026-06-21 15:44:08 -0300

    04-Angular avance com service workers, signals e SSR [ 02-Angular gerenciando estado com Signals ] 02-Signals e effects (efeitos)

--- lab-signals/src/app/app-routing.module.ts
+++ lab-signals/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignalsIntroComponent } from './signals-intro/signals-intro.component';
+import { EffectsComponent } from './effects/effects.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'elements', pathMatch: 'full' }, 
-  { path: 'intro', component: SignalsIntroComponent }, 
+  { path: '', redirectTo: 'elements', pathMatch: 'full' },
+  { path: 'intro', component: SignalsIntroComponent },
+  { path: 'effects', component: EffectsComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- lab-signals/src/app/app.module.ts
+++ lab-signals/src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignalsIntroComponent } from './signals-intro/signals-intro.component';
+import { EffectsComponent } from './effects/effects.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SignalsIntroComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SignalsIntroComponent, EffectsComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
