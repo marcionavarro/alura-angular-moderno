7c2d52c4d44cc5308ccc7b60c1d63571cef35197
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2026-06-23 22:13:00 -0300

    04-Angular avance com service workers, signals e SSR [ 02-Angular gerenciando estado com Signals ] 03-Otimização com signals e serviços

--- lab-signals/src/app/signals-intro/signals-intro.component.ts
+++ lab-signals/src/app/signals-intro/signals-intro.component.ts
@@ -1,10 +1,5 @@
 import { Component, computed, signal } from '@angular/core';
-
-interface Elemento {
-  nome: string;
-  simbolo: string;
-  numeroMassa: number;
-}
+import { Elemento, ElementoService } from '../elemento.service';
 
 @Component({
   selector: 'app-signals-intro',
@@ -12,25 +7,9 @@ interface Elemento {
   styleUrls: ['./signals-intro.component.css'],
 })
 export class SignalsIntroComponent {
-  elementoSelecionado = signal<Elemento | null>(null);
-
-  elementos: Elemento[] = [
-    { nome: 'Hidrogênio', simbolo: 'H', numeroMassa: 1 },
-    { nome: 'Carbono', simbolo: 'C', numeroMassa: 12 },
-    { nome: 'Nitrogênio', simbolo: 'N', numeroMassa: 14 },
-    { nome: 'Oxigênio', simbolo: 'O', numeroMassa: 16 },
-    { nome: 'Sódio', simbolo: 'Na', numeroMassa: 23 },
-    { nome: 'Cloro', simbolo: 'Cl', numeroMassa: 35 },
-  ];
+  constructor(protected elementoService: ElementoService) {}
 
   selecionarElemento(elemento: Elemento) {
-    this.elementoSelecionado.set(elemento);
+    this.elementoService.selecionarElemento(elemento);
   }
-
-  elementoInfo = computed(() => {
-    const elemento = this.elementoSelecionado();
-    return elemento
-      ? `Nome: ${elemento.nome}, Símbolo: ${elemento.simbolo}, Número de massa: ${elemento.numeroMassa}`
-      : 'Nenhum elemento selecionado';
-  });
 }
