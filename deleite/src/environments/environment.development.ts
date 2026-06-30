ecd9f63c9b1351319b2d16e60f48e16cc62704a6
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2026-06-30 00:07:21 -0300

    04-Angular avance com service workers, signals e SSR [ 03-Angular e Supabase otimize a sua aplicação com SSR ] 01-Integração com o Supabase

---
+++ deleite/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://datkixazsqcjnlhiwyim.supabase.co',
+    supabaseKey: 'sb_publishable_z57R4nI5-0W_u5KdRbnLig_tKSo-cc4',
+};
