a762b2fc93b040ef887add0845b9cb16bb6e340a
Marcio Navarro <marcionavarrodearaujo@gmail.com>
2026-07-13 20:33:36 -0300

    04-Angular avance com service workers, signals e SSR [ 04-Angular e Supabase refinando a aplicação com SSR ] 01-Desenvolvendo a lógica de compra

---
+++ deleite/src/app/components/cart/cart.component.ts
@@ -0,0 +1,45 @@
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CartItem } from '../../interfaces/cart_item';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-cart',
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatDividerModule,
+    MatIconModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatButtonModule,
+    RouterLink,
+  ],
+  templateUrl: './cart.component.html',
+  styleUrl: './cart.component.css',
+})
+export class CartComponent {
+  cartItems: CartItem[] = [];
+  total: number = 0;
+
+  getQuantities(currentQuantity: number): number[] {
+    const maxQuantity = Math.max(currentQuantity, 10);
+    return Array.from({ length: maxQuantity }, (_, i) => i + 1);
+  }
+
+  updateQuantity(productId: number, newQuantity: number) {}
+
+  removeItem(productId: number) {}
+
+  finalizePurchase() {}
+
+  continueShopping() {}
+}
--- deleite/src/app/components/header/header.component.ts
+++ deleite/src/app/components/header/header.component.ts
@@ -3,10 +3,17 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterLink,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
